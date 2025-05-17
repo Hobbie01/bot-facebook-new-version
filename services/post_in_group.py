ef42fc78f99528c3a778333d5074c4c929f9fc0d
Hobbie01 <61002652+Hobbie01@users.noreply.github.com>
2025-05-17 08:57:15 +0700

    update sleep

--- services/post_in_group.py
+++ services/post_in_group.py
@@ -35,7 +35,7 @@ def start_post_in_group(driver):
                     continue
                 driver.get(link)
                 print_log(f"ไปที่กลุ่ม {link}")
-                post = WebDriverWait(driver, 20).until(
+                post = WebDriverWait(driver, 120).until(
                 EC.presence_of_element_located(
                     (By.XPATH, '//div[contains(@class, "x1i10hfl x1ejq31n xd10rxx x1sy0etr x17r0tee x972fbf xcfux6l x1qhh985 xm0m39n x9f619 x1ypdohk xe8uvvx xdj266r x11i5rnm xat24cr x1mh8g0r x16tdsg8 x1hl2dhg xggy1nq x87ps6o x1lku1pv x1a2a7pz x6s0dn4 xmjcpbm x107yiy2 xv8uw2v x1tfwpuw x2g32xy x78zum5 x1q0g3np x1iyjqo2 x1nhvcw1 x1n2onr6 xt7dq6l x1ba4aug x1y1aw1k xn6708d xwib8y2 x1ye3gou")]')
                 )
@@ -43,7 +43,7 @@ def start_post_in_group(driver):
                 # 
                 # คลิกที่ span
                 post.click()
-                time.sleep(2)
+                time.sleep(10)
                 actions = ActionChains(driver)
                 sub_lines = comment.split("\n")
                 for sub_line in sub_lines:
@@ -52,7 +52,7 @@ def start_post_in_group(driver):
                 time.sleep(random.uniform(3, 10))  
                 # ส่ง RETURN (Enter) และ ESCAPE เพื่อปิดช่องคอมเมนต์
                 actions.send_keys(Keys.RETURN).perform()
-                image_buttons = WebDriverWait(driver, 10).until(
+                image_buttons = WebDriverWait(driver, 20).until(
     EC.presence_of_all_elements_located(
         (By.XPATH, '//div[contains(@class, "x6s0dn4") and contains(@class, "x78zum5") and contains(@class, "xl56j7k") and contains(@class, "x1n2onr6") and contains(@class, "x5yr21d") and contains(@class, "xh8yej3")]')
     )
@@ -67,11 +67,12 @@ def start_post_in_group(driver):
                     except Exception as e:
                             print_log(f"ไม่พบ input file ใน div นี้ {e}")
                 # ✅ กดปุ่มโพสต์
+                time.sleep(5)
                 post_button = WebDriverWait(driver, 40).until(
                     EC.presence_of_element_located((By.XPATH, '//div[@role="button" and @aria-label="โพสต์"]'))
                 )
                 post_button.click()
-                time.sleep(5)
+                time.sleep(20)
                 print_log("Post success")
 
             except Exception as e:
